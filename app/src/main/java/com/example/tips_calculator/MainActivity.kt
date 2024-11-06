c006c613a98b6129839dbec85c2806d37526c065
gabriel perninha <gabrielperninha.carvalho@gmail.com>
2024-11-06 14:10:18 -0300

    at2 : binding

--- app/src/main/java/com/example/tips_calculator/MainActivity.kt
+++ app/src/main/java/com/example/tips_calculator/MainActivity.kt
@@ -3,24 +3,45 @@ package com.example.tips_calculator
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.example.tips_calculator.databinding.ActivityMainBinding
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val editTotalConta = findViewById<TextInputEditText>(R.id.edit_total_conta)
-
-        val editNumeroPessoas = findViewById<TextInputEditText>(R.id.edit_numero_pessoas)
-
-        val btnCalcular = findViewById<Button>(R.id.btn_calcular)
-
-        btnCalcular.setOnClickListener {
-
-            println("agora ta certo")
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
 
+        binding.rbOption1.setOnCheckedChangeListener { _, isChecked ->
+            println("perna option 1: $isChecked")
+        }
+
+        binding.rbOption2.setOnCheckedChangeListener { _, isChecked ->
+            println("perna option 2: $isChecked")
+        }
+
+        binding.rbOption3.setOnCheckedChangeListener { _, isChecked ->
+            println("perna option 3:$isChecked")
+        }
+
+        binding.btnCalcular.setOnClickListener {
+
+            val valorTotal : Float = binding.editTotalConta.text.toString().toFloat()
+            val numeroPessoas : Float = binding.editNumeroPessoas.text.toString().toFloat()
+
+            val calculo = valorTotal / numeroPessoas
+
+            println(calculo)
+
+        }
+
+        binding.btbClean.setOnClickListener {
+
+            println("tô aqui tb!")
         }
 
     }
